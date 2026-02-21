7f0931f5b171d9e33b955bbdbbefe0cb9bd18b34
vkv211 <AKASH.20221COM0096@presidencyuniversity.in>
2026-02-21 20:03:15 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+function App() {
+  return (
+    <div style={{textAlign:"center", marginTop:"100px"}}>
+      <h1>Smart Civic Portal</h1>
+      <p>Crowdsourced Civic Issue Reporting System</p>
+      <button style={{
+        padding:"10px 20px",
+        fontSize:"18px",
+        backgroundColor:"#007bff",
+        color:"white",
+        border:"none",
+        borderRadius:"8px"
+      }}>
+        Report Issue
+      </button>
+    </div>
+  );
+}
+
+export default App;
